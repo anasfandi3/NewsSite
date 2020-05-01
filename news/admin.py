b368ff4b9491d703152b9a4f04d103ba6f8bb5a3
anasfandi3 <anasfandi3@gmail.com>
2020-05-01 15:34:18 +0300

    home template uygulamasi ve adminde resim upload, resim galerisi ve yuklenen resimlerin gosterilmesi

--- news/admin.py
+++ news/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
 
-from news.models import Category, News
+from news.models import Category, News, Images
 
+class NewsImageInline(admin.TabularInline):
+    model = Images
+    extra = 3
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title', 'status','image_tag']
+    readonly_fields = ('image_tag',)
     list_filter = ['status']
 
-admin.site.register(Category, CategoryAdmin)
-
-
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title', 'status','image_tag']
+    readonly_fields = ('image_tag',)
     list_filter = ['category']
+    inlines = [NewsImageInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'news', 'image']
+
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Images, ImagesAdmin)
--- news/models.py
+++ news/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
 
 class Category(models.Model):
     STATUS = (
         ('True', 'active'),
         ('False', 'passive'),
     )
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -17,6 +19,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
 
 class News(models.Model):
     STATUS = (
@@ -24,7 +29,7 @@ class News(models.Model):
         ('False', 'passive'),
     )
     category = models.ForeignKey(Category, on_delete= models.CASCADE)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=200)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -36,3 +41,15 @@ class News(models.Model):
     delete_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+class Images (models. Model):
+    news=models.ForeignKey(News, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return self.title
+
+
