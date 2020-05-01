6cfe2fcab52cbc1a6362192cc3f22a63eb18da01
anasfandi3 <anasfandi3@gmail.com>
2020-05-01 17:35:43 +0300

    settings modeli olusturulmasi ve favicon ayarlamasi

--- news/models.py
+++ news/models.py
@@ -38,7 +38,7 @@ class News(models.Model):
     slug = models.SlugField()
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
-    delete_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
     def image_tag(self):
