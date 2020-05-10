7a4eacabef5e03cd6eda1485e5c1b410e97692ad
anasfandi3 <anasfandi3@gmail.com>
2020-05-10 04:00:14 +0300

    about us ve contact sayfalarini eklemesi

--- NewsSite/urls.py
+++ NewsSite/urls.py
@@ -19,10 +19,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from home import views
+
 urlpatterns = [
     path('news/', include('news.urls')),
     path('home/', include('home.urls')),
     path('', include('home.urls')),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
--- home/views.py
+++ home/views.py
@@ -9,3 +9,14 @@ def index(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting' : setting}
     return render(request, 'index.html', context)
+
+
+def about(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page':'about'}
+    return render(request, 'about.html', context)
+
+def contact(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page':'contact'}
+    return render(request, 'contact.html', context)
