c153b2e4e153c6d3097847afcbe466e6a07d34a8
anasfandi3 <anasfandi3@gmail.com>
2020-05-11 16:41:15 +0300

    contact form message ayarlandi

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+
+from home.models import Setting, ContactFormMessage, ContactForm
 
 
 def index(request):
@@ -17,6 +19,20 @@ def about(request):
     return render(request, 'about.html', context)
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "your message had been sent successfully!")
+            return HttpResponseRedirect('/contact')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page':'contact'}
+    #form = ContactForm()
+    context = {'setting': setting, 'page': 'contact'}
     return render(request, 'contact.html', context)
+
